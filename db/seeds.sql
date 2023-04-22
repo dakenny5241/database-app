88e8c249fbff0aa36404fe8cb9b2a894927e6569
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-04-22 11:46:12 +0100

    seeds.sql employee added

--- db/seeds.sql
+++ db/seeds.sql
@@ -23,3 +23,20 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "kend", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chu", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ashley", "Rodi", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tup", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sunal", "Singh", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Salia", "Brown", 6, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES (Karah", "Lourd", 7, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Alli", 8, 7);  
